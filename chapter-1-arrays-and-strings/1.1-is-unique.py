06ff3c345034270a457adc55612dbe468a5d98a8
mike <78767272+mmilett14@users.noreply.github.com>
2021-07-28 16:10:10 -0500

    Problem solved

--- chapter-1-arrays-and-strings/1.1-is-unique.py
+++ chapter-1-arrays-and-strings/1.1-is-unique.py
@@ -1,1 +1,14 @@
 # Implement an algorithm to determine if a string has all unique characters.  What if you cannot use additional data structures?
+
+def find_unique_char(s):
+
+    used_letters = []
+
+    for i in range(0, len(s)):
+        if s[i] in used_letters:
+            return f"{s[i]} is a repeat!"
+        else:
+            used_letters.append(s[i])
+            
+    if len(used_letters) == len(s):
+        return 'there are no repeated letters in this string.'
