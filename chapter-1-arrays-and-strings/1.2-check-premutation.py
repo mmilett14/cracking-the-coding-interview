1b80ce5b9a7e8980e57b6d5eb7a460d294a518db
mike <78767272+mmilett14@users.noreply.github.com>
2021-07-29 15:37:13 -0500

    Problem solved!

--- chapter-1-arrays-and-strings/1.2-check-premutation.py
+++ chapter-1-arrays-and-strings/1.2-check-premutation.py
@@ -1,1 +1,23 @@
 # Given two strings, write a method to decide if one is a permutation of the other.
+
+def check_permutation(string0, string1):
+    
+    letter_match = 0
+    
+    if len(string0) != len(string1):
+        print("strings are different lengths - they are not permutations!")
+    
+    else:
+        for i in range(0,len(string0)):
+            
+            for j in range(0,len(string1)):
+            
+                if string0[i] == string1[j]:
+                    letter_match += 1
+                    break
+                
+        if letter_match == len(string0):
+            print("congratulations, it's a permutation!")
+        
+        else:
+            print("they are not permutations!")
