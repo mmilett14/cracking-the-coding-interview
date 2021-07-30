59043f4bcfd212f360515de35018bb2d4e9c1540
mike <78767272+mmilett14@users.noreply.github.com>
2021-07-30 16:00:26 -0500

    not quite there....

--- chapter-1-arrays-and-strings/1.3-URLify.py
+++ chapter-1-arrays-and-strings/1.3-URLify.py
@@ -1,1 +1,35 @@
 # Write a method to replace all spaces in a string with '%20'.
+
+def urlify(string):
+    
+    space_list = []
+    
+    for i in range(0, len(string0)):
+        if string[i] == ' ':
+            space_list.append(i)
+            
+    string_start = 0
+    string_end = len(string)-1
+    
+    string_final = ''
+    
+    print(space_list)
+    
+    for i in range(0, len(space_list)):
+        
+        if i == 0:
+            string_part = string[0:space_list[i]]
+            
+            string_final = string_final + string_part + '%20' + string[space_list[i]-1:space_list[i+1]]
+        
+        elif i == len(space_list)-1:
+            string_part = string[space_list[i]-1:len(string)]
+            
+            string_final = string_final + '%20' + string_part
+            
+        else:
+            string_part = string[space_list[i]-1:space_list[i+1]]
+            
+            string_final = string_final + '%20' + string_part
+    
+    print(string_final)
