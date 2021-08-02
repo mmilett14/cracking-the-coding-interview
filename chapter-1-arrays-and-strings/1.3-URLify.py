dbd25376d38779cf32314be4024aeedd00125248
mike <78767272+mmilett14@users.noreply.github.com>
2021-08-02 16:02:54 -0500

    it works!

--- chapter-1-arrays-and-strings/1.3-URLify.py
+++ chapter-1-arrays-and-strings/1.3-URLify.py
@@ -3,41 +3,31 @@
 def urlify(string):
     
     space_list = []
-    
-    for i in range(0, len(string0)):
+
+    for i in range(0, len(string)):
         if string[i] == ' ':
             space_list.append(i)
             
     string_start = 0
     string_end = len(string)-1
-    
+
     string_final = ''
-    
-    print(space_list)
-    
-    for i in range(0, len(space_list)):
+
+    for i in range(0, len(space_list)+1):
         
         if i == 0:
             string_part = string[0:space_list[i]]
             
-            string_final = string_final + string_part + '%20' + string[space_list[i]-1:space_list[i+1]]
+            string_final = string_final + string_part + '%20'
             
-            print(string_final)
+        elif i == len(space_list):
+            string_part = string[space_list[i-1]+1:len(string)]
             
-            break
-        
-        elif i == len(space_list)-1:
-            string_part = string[space_list[i]-1:len(string)]
-            
-            string_final = string_final + '%20' + string_part
-            
-            print(string_final)
+            string_final = string_final + string_part
             
         else:
             string_part = string[space_list[i-1]+1:space_list[i]]
             
-            string_final = string_final + '%20' + string_part
-            
-            print(string_final)
-    
+            string_final = string_final + string_part + '%20'
+
     print(string_final)
