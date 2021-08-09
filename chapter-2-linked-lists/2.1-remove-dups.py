a0bdc165ec8b96adb999c5aaa4b10620428e764a
mike <78767272+mmilett14@users.noreply.github.com>
2021-08-09 16:12:22 -0500

    initialize linked list

--- chapter-2-linked-lists/2.1-remove-dups.py
+++ chapter-2-linked-lists/2.1-remove-dups.py
@@ -1,1 +1,27 @@
-# Write code to remove duplicates from an unsorted linked list.
+### Write code to remove duplicates from an unsorted linked list.
+
+# Initialize linked list:
+
+class Node:
+  
+  def __init__(self, data):
+    self.data = data
+    self.next = None
+    
+class LinkedList:
+  
+  def __init__(self):
+    self.head = None
+    
+llist = LinkedList()
+
+llist.head = Node(1)
+second = Node(2)
+third = Node(3)
+fourth = Node(4)
+
+llist.head.next = second
+
+second.next = third
+
+third.next = fourth
