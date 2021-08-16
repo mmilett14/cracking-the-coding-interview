28b55e1517dbe70602d115483d8f57728d5c2079
mike <78767272+mmilett14@users.noreply.github.com>
2021-08-16 16:37:53 -0500

    added fib function with memoization

--- chapter-8-recursion-and-dynamic-programming/fibonacci.py
+++ chapter-8-recursion-and-dynamic-programming/fibonacci.py
@@ -5,3 +5,19 @@ def fibonacci(i):
         return 1
     else:
         return fibonacci(i - 1)  + fibonacci(i-2)
+    
+def fib_memo(i):
+    result = 0
+    memo = [None] * (i+1)
+    
+    if memo[i] != None:
+        return memo[i]
+    
+    elif (i == 1) | (i == 2):
+        result = 1
+        
+    else:
+        result = fib_memo(i-1) + fib_memo(i-2)
+        
+    memo[i] = result
+    return result
