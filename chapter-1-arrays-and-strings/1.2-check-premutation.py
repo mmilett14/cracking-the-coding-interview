b30acf7bc6b774dca76fe3d98301f83699c60916
mike <78767272+mmilett14@users.noreply.github.com>
2021-09-18 14:01:59 -0500

    found better solution

--- chapter-1-arrays-and-strings/1.2-check-premutation.py
+++ chapter-1-arrays-and-strings/1.2-check-premutation.py
@@ -21,3 +21,30 @@ def check_permutation(string0, string1):
         
         else:
             print("they are not permutations!")
+
+def check_permutation_better(str0, str1):
+
+    str_dict0 = {}
+    str_dict1 = {}
+    
+    for i in range(0, len(str0)):
+
+        if str0[i] not in str_dict0:
+            str_dict0[str0[i]] = 1
+
+        elif str0[i] in str_dict0:
+            str_dict0[str0[i]] += 1 
+    
+    for i in range(0, len(str1)):
+
+        if str1[i] not in str_dict1:
+            str_dict1[str1[i]] = 1
+
+        elif str1[i] in str_dict1:
+            str_dict1[str1[i]] += 1 
+    
+    if str_dict0 == str_dict1:
+        print('they match!')
+    
+    else:
+        print('they dont match!')
